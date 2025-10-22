186501bb2920879b9e862353f79fac376c8c53dd
Herie <thomas.herie@etud.u-picardie.fr>
2025-10-22 19:32:27 +0200

    creation de la page d'accueil avec un titre ses 3 boutons et son style adapté

--- src/main/java/com/simplerp/morpion/accueil/AccueilController.java
+++ src/main/java/com/simplerp/morpion/accueil/AccueilController.java
@@ -1,5 +1,18 @@
 package com.simplerp.morpion.accueil;
 
+import javafx.event.ActionEvent;
+
 public class AccueilController {
 
+    public void onJoueur1(ActionEvent event) {
+        System.out.println("Bouton 1 Joueur cliqué !");
+    }
+
+    public void onJoueur2(ActionEvent event) {
+        System.out.println("Bouton 2 Joueurs cliqué !");
+    }
+
+    public void onScores(ActionEvent event) {
+        System.out.println("Bouton Scores cliqué !");
+    }
 }
